2635d52c36e1d082c9b7f7baf8a287cad3f74b58
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-11-07 17:03:34 +0300

    Start writing tests for CalculatorStringExpression

---
+++ test/home_work_3/calcs/CalculatorStringExpressionTest.java
@@ -0,0 +1,18 @@
+package home_work_3.calcs;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorStringExpressionTest {
+    @Test
+    @DisplayName("4.1 + 15 * 7 + (28 / 5) ^ 2")
+    void sum1(){
+        String expr = "4.1 + 15 * 7 + (28 / 5) ^ 2";
+        CalculatorStringExpression calc = new CalculatorStringExpression(expr);
+        double result = calc.calculate();
+        assertEquals(140.46, result, 1e-6);
+    }
+
+}
