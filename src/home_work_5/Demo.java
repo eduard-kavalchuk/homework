d5605135aab796cdc78538a50c6c76509428d5f6
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-11-18 17:15:25 +0300

    Initial random string generator

---
+++ src/home_work_5/Demo.java
@@ -0,0 +1,12 @@
+package home_work_5;
+
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/home_work_5/PersonFactory.java
+++ src/home_work_5/PersonFactory.java
@@ -1,5 +1,7 @@
 package home_work_5;
 
+import java.util.Random;
+
 public class PersonFactory {
     public static Person createWithRandomName(String nick, String password) {
         return new Person(nick, password, "abc");
@@ -12,5 +14,16 @@ public class PersonFactory {
     }
     public static Person createWithNameFromFile(String nick, String password) {
         return new Person(nick, password, "abc");
+    }
+    private static String randomStringGenerator(int min, int max) {
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+        int utf8Code = random.nextInt(max - min) + min;
+        builder.appendCodePoint(utf8Code - 32);
+        for (int i = 0; i < 9; i++) {
+            utf8Code = random.nextInt(max - min) + min;
+            builder.appendCodePoint(utf8Code);
+        }
+        return builder.toString();
     }
 }
