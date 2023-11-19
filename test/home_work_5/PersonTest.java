cec908041de22affd99bfd5c1112c70078ac8de0
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-11-19 20:01:02 +0300

    Some refactoring

--- test/home_work_5/PersonTest.java
+++ test/home_work_5/PersonTest.java
@@ -10,22 +10,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PersonTest {
+    private static final String LATIN_NICKNAME = "nickname";
+    private static final String LATIN_NAME = "name";
+    private static final String CYRILLIC_NICKNAME = "Этокличка";
+    private static final String CYRILLIC_NAME = "Виталий";
+    private static final String PASSWORD = "qwerty";
+    private static final String PATH = String.join(File.separator, "test", "home_work_5", "names.txt");
     @Test
     @DisplayName("Person::createWithRandomLatinName()")
     public void createWithRandomLatinName() {
-        int len;
-        int code;
-        String name;
-        String nickname = "nickname";
-        String password = "qwerty";
-        Person person = PersonFactory.createWithRandomLatinName(nickname, password);
-        assertEquals(nickname, person.getNick());
-        assertEquals(password, person.getPassword());
-        len = person.getName().length();
+        Person person = PersonFactory.createWithRandomLatinName(LATIN_NICKNAME, PASSWORD);
+        assertEquals(LATIN_NICKNAME, person.getNick());
+        assertEquals(PASSWORD, person.getPassword());
+        int len = person.getName().length();
         assertTrue(5 <= len && len <= 10);
-        name = person.getName();
+        String name = person.getName();
         for (int i = 0; i < name.length(); i++) {
-            code = Character.codePointAt(name, i);
+            int code = Character.codePointAt(name, i);
             if (i == 0)
                 assertTrue(65 <= code && code <= 90);
             else assertTrue(97 <= code && code <= 122);
@@ -34,19 +35,14 @@ public class PersonTest {
     @Test
     @DisplayName("Person::createWithRandomLatinNickname()")
     public void createWithRandomLatinNickname() {
-        int len;
-        int code;
-        String nickname;
-        String name = "name";
-        String password = "qwerty";
-        Person person = PersonFactory.createWithRandomLatinNickname(password, name);
-        assertEquals(name, person.getName());
-        assertEquals(password, person.getPassword());
-        len = person.getNick().length();
+        Person person = PersonFactory.createWithRandomLatinNickname(PASSWORD, LATIN_NAME);
+        assertEquals(LATIN_NAME, person.getName());
+        assertEquals(PASSWORD, person.getPassword());
+        int len = person.getNick().length();
         assertTrue(5 <= len && len <= 10);
-        nickname = person.getNick();
+        String nickname = person.getNick();
         for (int i = 0; i < nickname.length(); i++) {
-            code = Character.codePointAt(nickname, i);
+            int code = Character.codePointAt(nickname, i);
             if (i == 0)
                 assertTrue(65 <= code && code <= 90);
             else assertTrue(97 <= code && code <= 122);
@@ -55,19 +51,14 @@ public class PersonTest {
     @Test
     @DisplayName("Person::createWithRandomRussianName()")
     public void createWithRandomRussianName() {
-        int len;
-        int code;
-        String name;
-        String nickname = "кличка";
-        String password = "qwerty";
-        Person person = PersonFactory.createWithRandomRussianName(nickname, password);
-        assertEquals(nickname, person.getNick());
-        assertEquals(password, person.getPassword());
-        len = person.getName().length();
+        Person person = PersonFactory.createWithRandomRussianName(CYRILLIC_NICKNAME, PASSWORD);
+        assertEquals(CYRILLIC_NICKNAME, person.getNick());
+        assertEquals(PASSWORD, person.getPassword());
+        int len = person.getName().length();
         assertTrue(5 <= len && len <= 10);
-        name = person.getName();
+        String name = person.getName();
         for (int i = 0; i < name.length(); i++) {
-            code = Character.codePointAt(name, i);
+            int code = Character.codePointAt(name, i);
             if (i == 0)
                 assertTrue(1040 <= code && code <= 1071);
             else assertTrue(1072 <= code && code <= 1103);
@@ -76,19 +67,14 @@ public class PersonTest {
     @Test
     @DisplayName("Person::createWithRandomRussianNickname()")
     public void createWithRandomRussianNickname() {
-        int len;
-        int code;
-        String nickname;
-        String name = "Этоимя";
-        String password = "qwerty";
-        Person person = PersonFactory.createWithRandomRussianNickname(password, name);
-        assertEquals(name, person.getName());
-        assertEquals(password, person.getPassword());
-        len = person.getNick().length();
+        Person person = PersonFactory.createWithRandomRussianNickname(PASSWORD, CYRILLIC_NAME);
+        assertEquals(CYRILLIC_NAME, person.getName());
+        assertEquals(PASSWORD, person.getPassword());
+        int len = person.getNick().length();
         assertTrue(5 <= len && len <= 10);
-        nickname = person.getNick();
+        String nickname = person.getNick();
         for (int i = 0; i < nickname.length(); i++) {
-            code = Character.codePointAt(nickname, i);
+            int code = Character.codePointAt(nickname, i);
             if (i == 0)
                 assertTrue(1040 <= code && code <= 1071);
             else assertTrue(1072 <= code && code <= 1103);
@@ -97,17 +83,13 @@ public class PersonTest {
     @Test
     @DisplayName("Person::createWithRandomRealName()")
     public void createWithRandomRealName() {
-        int code;
-        String name;
-        String nickname = "nickname";
-        String password = "qwerty";
-        Person person = PersonFactory.createWithRandomRealName(nickname, password);
-        assertEquals(nickname, person.getNick());
-        assertEquals(password, person.getPassword());
+        Person person = PersonFactory.createWithRandomRealName(LATIN_NICKNAME, PASSWORD);
+        assertEquals(LATIN_NICKNAME, person.getNick());
+        assertEquals(PASSWORD, person.getPassword());
         // Length of name is not asserted since it can be other than [5, 10]
-        name = person.getName();
+        String name = person.getName();
         for (int i = 0; i < name.length(); i++) {
-            code = Character.codePointAt(name, i);
+            int code = Character.codePointAt(name, i);
             if (i == 0)
                 assertTrue(65 <= code && code <= 90);
             else assertTrue(97 <= code && code <= 122);
@@ -116,19 +98,15 @@ public class PersonTest {
     @Test
     @DisplayName("Person::createWithRandomRealNickname()")
     public void createWithRandomRealNickname() {
-        int len;
-        int code;
         String nickname;
-        String name = "name";
-        String password = "qwerty";
-        Person person = PersonFactory.createWithRandomRealNickname(password, name);
-        assertEquals(name, person.getName());
-        assertEquals(password, person.getPassword());
-        len = person.getNick().length();
+        Person person = PersonFactory.createWithRandomRealNickname(PASSWORD, LATIN_NAME);
+        assertEquals(LATIN_NAME, person.getName());
+        assertEquals(PASSWORD, person.getPassword());
+        int len = person.getNick().length();
         assertTrue(5 <= len && len <= 10);
         nickname = person.getNick();
         for (int i = 0; i < nickname.length(); i++) {
-            code = Character.codePointAt(nickname, i);
+            int code = Character.codePointAt(nickname, i);
             if (i == 0)
                 assertTrue(65 <= code && code <= 90);
             else assertTrue(97 <= code && code <= 122);
@@ -137,18 +115,13 @@ public class PersonTest {
     @Test
     @DisplayName("Person::createWithRandomNameFromFile()")
     public void createWithRandomNameFromFile() throws IOException {
-        int code;
-        String name;
-        String nickname = "nickname";
-        String password = "qwerty";
-        String path = String.join(File.separator, "test", "home_work_5", "names.txt");
-        Person person = PersonFactory.createWithRandomNameFromFile(nickname, password, path);
-        assertEquals(nickname, person.getNick());
-        assertEquals(password, person.getPassword());
+        Person person = PersonFactory.createWithRandomNameFromFile(LATIN_NICKNAME, PASSWORD, PATH);
+        assertEquals(LATIN_NICKNAME, person.getNick());
+        assertEquals(PASSWORD, person.getPassword());
         // Length of name is not asserted since it can be other than [5, 10]
-        name = person.getName();
+        String name = person.getName();
         for (int i = 0; i < name.length(); i++) {
-            code = Character.codePointAt(name, i);
+            int code = Character.codePointAt(name, i);
             if (i == 0)
                 assertTrue(65 <= code && code <= 90);
             else assertTrue(97 <= code && code <= 122);
@@ -157,20 +130,14 @@ public class PersonTest {
     @Test
     @DisplayName("Person::createWithRandomNicknameFromFile()")
     public void createWithRandomNicknameFromFile() throws IOException {
-        int len;
-        int code;
-        String nickname;
-        String name = "name";
-        String password = "qwerty";
-        String path = String.join(File.separator, "test", "home_work_5", "names.txt");
-        Person person = PersonFactory.createWithRandomNicknameFromFile(name, password, path);
-        assertEquals(name, person.getName());
-        assertEquals(password, person.getPassword());
-        len = person.getNick().length();
+        Person person = PersonFactory.createWithRandomNicknameFromFile(LATIN_NAME, PASSWORD, PATH);
+        assertEquals(LATIN_NAME, person.getName());
+        assertEquals(PASSWORD, person.getPassword());
+        int len = person.getNick().length();
         assertTrue(5 <= len && len <= 10);
-        nickname = person.getNick();
+        String nickname = person.getNick();
         for (int i = 0; i < nickname.length(); i++) {
-            code = Character.codePointAt(nickname, i);
+            int code = Character.codePointAt(nickname, i);
             if (i == 0)
                 assertTrue(65 <= code && code <= 90);
             else assertTrue(97 <= code && code <= 122);
