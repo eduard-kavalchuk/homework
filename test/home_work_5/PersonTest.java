a9d64e80fd87fd3b4ae51a50d169f819a6cf3102
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-11-19 17:44:15 +0300

    Continue testing Person

--- test/home_work_5/PersonTest.java
+++ test/home_work_5/PersonTest.java
@@ -49,4 +49,46 @@ public class PersonTest {
             else assertTrue(97 <= code && code <= 122);
         }
     }
+    @Test
+    @DisplayName("Person::createWithRandomRussianName()")
+    public void createWithRandomRussianName() {
+        int len;
+        int code;
+        String name;
+        String nickname = "кличка";
+        String password = "qwerty";
+        Person person = PersonFactory.createWithRandomRussianName(nickname, password);
+        assertEquals(nickname, person.getNick());
+        assertEquals(password, person.getPassword());
+        len = person.getName().length();
+        assertTrue(5 <= len && len <= 10);
+        name = person.getName();
+        for (int i = 0; i < name.length(); i++) {
+            code = Character.codePointAt(name, i);
+            if (i == 0)
+                assertTrue(1040 <= code && code <= 1071);
+            else assertTrue(1072 <= code && code <= 1103);
+        }
+    }
+    @Test
+    @DisplayName("Person::createWithRandomRussianNickname()")
+    public void createWithRandomRussianNickname() {
+        int len;
+        int code;
+        String nickname;
+        String name = "Этоимя";
+        String password = "qwerty";
+        Person person = PersonFactory.createWithRandomRussianNickname(password, name);
+        assertEquals(name, person.getName());
+        assertEquals(password, person.getPassword());
+        len = person.getNick().length();
+        assertTrue(5 <= len && len <= 10);
+        nickname = person.getNick();
+        for (int i = 0; i < nickname.length(); i++) {
+            code = Character.codePointAt(nickname, i);
+            if (i == 0)
+                assertTrue(1040 <= code && code <= 1071);
+            else assertTrue(1072 <= code && code <= 1103);
+        }
+    }
 }
