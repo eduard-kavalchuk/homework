357c4e36658670d6df49a375ea404388dc25a9ca
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-11-19 21:27:27 +0300

    Add description of NameGenerator

--- src/home_work_5/helpers/NameGenerator.java
+++ src/home_work_5/helpers/NameGenerator.java
@@ -2,7 +2,20 @@ package home_work_5.helpers;
 
 import java.util.Random;
 
+/**
+ * Provides helper methods for generating random capitalized Latin and Cyrillic
+ * character sequences.
+ * */
 public class NameGenerator {
+    /**
+     * Generates a random character sequence in a target alphabet.
+     * Target alphabet is specified by providing first and last character codes of this alphabet
+     * as per UTF-8 table.
+     * First character is capitalized. The returned sequence is 5 to 10 characters long.
+     * @param min starting UTF-8 character code
+     * @param max finishing UTF-8 character code
+     * @return Random string in a target alphabet
+     * */
     private static String randomStringGenerator(int min, int max) {
         StringBuilder builder = new StringBuilder();
         Random random = new Random();
@@ -15,9 +28,17 @@ public class NameGenerator {
         }
         return builder.toString();
     }
+
+    /**
+     * Generates a random capitalized Latin character sequence
+     * */
     public static String getRandomLatinString() {
         return randomStringGenerator(97, 122);
     }
+
+    /**
+     * Generates a random capitalized Cyrillic character sequence
+     * */
     public static String getRandomCyrillicString() {
         return randomStringGenerator(1072, 1103);
     }
