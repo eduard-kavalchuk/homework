78978b7d984c909023ed36a9453f56893a84f9a3
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-11-22 22:04:34 +0300

    Implement custom hashCode, equals and compareTo

--- src/home_work_5/Person.java
+++ src/home_work_5/Person.java
@@ -4,13 +4,15 @@ import home_work_5.helpers.PersonComparatorByPasswordLength;
 import home_work_5.helpers.PersonComparatorByPasswordLengthAndNickname;
 import home_work_5.helpers.Sort;
 
+import java.util.Objects;
+
 /**
  * DTO class Person.
  * Since DTO classes are designed to transfer immutable objects all fields of this class are declared {@code final}.
  * Setters are not implemented since field values are assigned once during constructor call.
  * To create specific Person instances use PersonFactory factory class.
  * */
-public class Person {
+public class Person implements Comparable<Person> {
     private final String nick;
     private final String password;
     private final String name;
@@ -38,5 +40,45 @@ public class Person {
      * */
     public static void sortByPasswordLengthAndNickname(Person[] people) {
         Sort.sort(people, new PersonComparatorByPasswordLengthAndNickname());
+    }
+
+    /**
+     * Redefine {@code equals} method
+     * @param other instance to be compared with
+     * @return {@code true} if {@code other} is the same instance and {@code false} otherwise
+     * */
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null) {
+            return false;
+        }
+        if (!(other instanceof Person person)) {
+            return false;
+        }
+        return person.name.equals(this.name) && person.nick.equals(this.nick) && person.password.equals(this.password);
+    }
+
+    /**
+     * @return a hash value of the sequence of object's fields
+     * */
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, nick, password);
+    }
+
+    /**
+     * Implements {@code Comparable} interface
+     * */
+    public int compareTo(Person o) {
+        if (equals(o)) {
+            return 0;
+        }
+        else if(hashCode() > o.hashCode()) {
+            return 1;
+        }
+        else return -1;
     }
 }
