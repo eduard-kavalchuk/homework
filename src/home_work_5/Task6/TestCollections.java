06bbaa1bcdedaf27d7df089682d57506cf15c384
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-11-23 18:42:53 +0300

    Use polymorphic approach in Task6

--- src/home_work_5/Task6/TestCollections.java
+++ src/home_work_5/Task6/TestCollections.java
@@ -5,16 +5,17 @@ package home_work_5.Task6;
  * */
 public class TestCollections {
     public static void main(String[] args) {
-        ArrayListCollection alc = new ArrayListCollection(1000);
+        // Применяем полиморфизм
+        PeopleCollectionsManipulator alc = new ArrayListCollection(1000);
         alc.calculateTimes();
 
-        LinkedListCollection llc = new LinkedListCollection(1000);
+        PeopleCollectionsManipulator llc = new LinkedListCollection(1000);
         llc.calculateTimes();
 
-        HashSetCollection hsc = new HashSetCollection(1000);
+        PeopleCollectionsManipulator hsc = new HashSetCollection(1000);
         hsc.calculateTimes();
 
-       TreeSetCollection tsc = new TreeSetCollection(1000);
-       tsc.calculateTimes();
+        PeopleCollectionsManipulator tsc = new TreeSetCollection(1000);
+        tsc.calculateTimes();
     }
 }
