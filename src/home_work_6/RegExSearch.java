4fb205f4bac69ac87b22c98956b2724c3a676b32
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-11-29 22:51:06 +0300

    Fix RegExSearch

--- src/home_work_6/RegExSearch.java
+++ src/home_work_6/RegExSearch.java
@@ -6,22 +6,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExSearch implements ISearchEngine {
-    // ищем отдельно стоящее слово
     public long search(String text, String word) {
-        String firstPositionRe = "^" + word + "\\s+";
-        String middlePositionRe = "\\s+" + word + "\\s+";
-        String endPositionRe = "\\s+" + word + "$";
-        String pattern = String.join("|", firstPositionRe, middlePositionRe, endPositionRe);
+        String pattern = "\\b" + word + "\\b";
         Pattern stringPattern = Pattern.compile(pattern);
         Matcher m = stringPattern.matcher(text);
         long counter = 0;
-        while (m.find())
+        while (m.find()) {
             ++counter;
+        }
         return counter;
     }
 
     public static void main(String[] args) {
-        String text = "a b ce d ef";
+        String text = "ef  ef ce ef ef";
         String word = "ef";
         RegExSearch rs = new RegExSearch();
         System.out.println(rs.search(text, word));
