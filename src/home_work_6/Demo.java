56e50b9692181069e93979ac4d06f5da4eb836ac
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-11-29 18:57:13 +0300

    Draft version of file2String

--- src/home_work_6/Demo.java
+++ src/home_work_6/Demo.java
@@ -2,33 +2,28 @@ package home_work_6;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Demo {
     private static final String PUNCTUATION_MARKS = "[.,;:!?\'\"]";
-    private static final String HYPHEN = " -- ";
-    public static String file2String() {
-        String path = String.join(File.separator, System.getProperty("user.dir"), "resources", "book.txt");
+    private static final String path = String.join(File.separator, System.getProperty("user.dir"), "resources", "book.txt");
+    public static String file2String(String path) {
         StringBuilder sb = new StringBuilder();
-        String line;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-            while ((line = reader.readLine()) != null){
+            String line;
+            while ((line = reader.readLine()) != null) {
                 if (line.isEmpty())
                     sb.append(" ");
-                else sb.append(line);
+                else sb.append(line
+                        .replaceAll("\\s[^\\p{L}]+\\s", " ")  // убрать все слова где нет букв (любого алфавита)
+                        .replaceAll(PUNCTUATION_MARKS, " "));       // убрать знаки пунктуации
             }
 
-            // Replace with a builder pattern!
-//            line = sb.toString().replaceAll("\\s+[^a-zA-Z]+\\s+", " ");
-//            line = sb.toString().replaceAll(PUNCTUATION_MARKS, " ");
-//            line = line.replaceAll(HYPHEN, " ");
-//            line = line.replaceAll(HYPHEN, " ");
-//            line = line.replaceAll("[0-9]-", " ");
-//            line = line.replaceAll("\\s+[0-9]+\\s+", " ");
-//            line = line.replaceAll(" - ", " ");
-//            line = line.replaceAll("\\s{2,}", " ");
             return sb.toString();
+
         } catch (FileNotFoundException e){
             throw new RuntimeException("Файл не найден.");
         } catch (IOException e) {
@@ -37,20 +32,10 @@ public class Demo {
     }
 
     public static void main(String[] args) {
-        String s = "Источник: Издательство: 4-x Захаров, 2007 г. Твердый переплет, 800 стр. ISBN   978-5-8159-0748-5 Тираж: 5000 экз.";
-        String[] array = s.split(" ");
-        StringBuilder sb = new StringBuilder();
-        for (String str : array) {
-            for (int i = 0; i < str.length(); i++) {
-                if (Character.UnicodeBlock.of(str.charAt(i)).equals(Character.UnicodeBlock.CYRILLIC)) {
-                    sb.append(str);
-                    sb.append(" ");
-                    break;
-                }
-            }
-        }
-
-        System.out.println(sb.toString());
+        String s = file2String(path);
+        s = s.substring(100, 500);
+        Stream<String> stream = Arrays.stream(s.split( "\\s+" ));
+        stream.forEach(System.out::println);
 
     }
 }
