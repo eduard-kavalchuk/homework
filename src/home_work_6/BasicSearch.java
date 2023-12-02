422514dc1e463cba9e2be9640c6c759c568001f9
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-12-02 12:34:56 +0300

    Add decorator

--- src/home_work_6/BasicSearch.java
+++ src/home_work_6/BasicSearch.java
@@ -5,40 +5,44 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Demo {
-    private static final String PUNCTUATION_MARKS = "[.,;:!?\'\"]";
-    private static final String path = String.join(File.separator, System.getProperty("user.dir"), "resources", "book.txt");
-    public static String file2String(String path) {
+public class BasicSearch {
+    public static final String path = String.join(File.separator, System.getProperty("user.dir"), "resources", "book.txt");
+    public String get(String path) {
         StringBuilder sb = new StringBuilder();
-
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 if (line.isEmpty())
                     sb.append(" ");
-                else sb.append(line
-                        .replaceAll("\\s[^\\p{L}]+\\s", " ")  // убрать все слова где нет букв (любого алфавита)
-                        .replaceAll(PUNCTUATION_MARKS, " "));       // убрать знаки пунктуации
+                else sb.append(line);
             }
         } catch (FileNotFoundException e){
             throw new RuntimeException("Файл не найден.");
         } catch (IOException e) {
             throw new RuntimeException("Ошибка чтения файла: " + e.getMessage());
         }
-
         return sb.toString();
     }
 
-    public static int getUniqueWordsNumber() {
-        Stream<String> stream = Arrays.stream(file2String(path).split( "\\s+" ));
+    public int getUniqueWordsNumber(String doc) {
+        Stream<String> stream = Arrays.stream(doc.split( "\\s+" ));
         Set<String> set = new HashSet<>();
         stream.forEach(set::add);
         return set.size();
     }
 
-    public static void printMostUsedWords(int N) {
-        Map<String, Integer> unsortedMap = getMostFrequentWords();
-        System.out.println(unsortedMap.size());
+    public int getWordCount(String doc, String word) {
+        Map<String, Integer> map =  getMostFrequentWords(doc);
+        try {
+            return map.get(word);
+        } catch (NullPointerException e) {
+            return 0;
+        }
+    }
+
+    public void printMostUsedWords(String doc, int N) {
+        Map<String, Integer> unsortedMap = getMostFrequentWords(doc);
+        System.out.println("Всего уникальных слов: " + unsortedMap.size());
         LinkedHashMap<String, Integer> sortedMap = unsortedMap.entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
@@ -54,9 +58,9 @@ public class Demo {
         }
     }
 
-    public static Map<String, Integer> getMostFrequentWords() {
+    public Map<String, Integer> getMostFrequentWords(String doc) {
         Map<String, Integer> map = new HashMap<>();
-        Stream<String> stream = Arrays.stream(file2String(path).split( "\\s+" ));
+        Stream<String> stream = Arrays.stream(doc.split( "\\s+" ));
         final Integer[] e = new Integer[1];
         stream.forEach(entry -> map.put(entry, (e[0] = map.get(entry)) == null ? 1 : e[0] + 1));
         return map;
---
+++ src/home_work_6/SearchEnginePunctuationNormalizer.java
@@ -0,0 +1,26 @@
+package home_work_6;
+
+public class SearchEnginePunctuationNormalizer extends BasicSearch {
+    private static final String PUNCTUATION_MARKS = "[.,;:!?\'\"]";
+    private final BasicSearch bs;
+    public SearchEnginePunctuationNormalizer(BasicSearch bs) {
+        this.bs = bs;
+    }
+    public String get(String path) {
+        String doc = bs.get(path);
+        return  doc
+                .replaceAll("\\s[^\\p{L}]+\\s", " ") // убрать все слова где нет букв (любого алфавита)
+                .replaceAll(PUNCTUATION_MARKS, " ") // убрать знаки пунктуации
+                .trim()  // remote leading and trailing spaces
+                .replaceAll("\\s+", " ");  // remove all consecutive escapes
+    }
+
+    public static void main(String[] args) {
+        SearchEnginePunctuationNormalizer o = new SearchEnginePunctuationNormalizer(new BasicSearch());
+        String doc = o.get(path);
+//        System.out.println(doc.substring(20000, 20200));
+//        System.out.println(o.getUniqueWordsNumber(doc));
+//        o.printMostUsedWords(doc, 20);
+        System.out.println(o.getWordCount(doc, "и."));
+    }
+}
