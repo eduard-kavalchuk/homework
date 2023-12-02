fc52f8de3327936d55ab71a4a178e6136bcc4f6b
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-12-02 13:41:20 +0300

    Start testing with Demo class

--- src/home_work_6/Demo.java
+++ src/home_work_6/Demo.java
@@ -5,10 +5,10 @@ import home_work_6.api.ISearchEngine;
 public class Demo {
     public static void main(String[] args) {
         BasicSearch bs = new BasicSearch();
-        SearchEnginePunctuationNormalizer o = new SearchEnginePunctuationNormalizer(bs);
-        final String doc = o.get(BasicSearch.path);
         ISearchEngine es = new EasySearch();
         ISearchEngine re = new RegExSearch();
+        SearchEnginePunctuationNormalizer o = new SearchEnginePunctuationNormalizer(bs);
+        final String doc = o.get(BasicSearch.path);
 
         String word = "мир";
         System.out.println(re.search(doc, word));
