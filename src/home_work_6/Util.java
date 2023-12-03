0f50f8affff3a0a002981aac1c9929746786e12e
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-12-03 22:20:41 +0300

    Read directory

--- src/home_work_6/Util.java
+++ src/home_work_6/Util.java
@@ -1,6 +1,9 @@
 package home_work_6;
 
 import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Util {
     public static String src = String.join(File.separator, System.getProperty("user.dir"), "resources", "book.txt");
@@ -20,11 +23,29 @@ public class Util {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        for (int i = 1; i < 3; i++) {
+    public static void generate(int n_books) {
+        for (int i = 1; i < n_books; i++) {
             String dst = String.join(File.separator, Util.dst, i + ".txt");
             copyFile(Util.src, dst, 100 * i, 100);
         }
+    }
 
+    public static List<String> listFilesUsingJavaIO(String dir) {
+        return Stream.of(new File(dir).listFiles())
+                .filter(file -> !file.isDirectory())
+                .map(File::getName)
+                .collect(Collectors.toList());
+    }
+    public static void main(String[] args) {
+        List<String> filenames = listFilesUsingJavaIO(dst);
+        Collections.sort(filenames);
+        Map<Integer, String> sortedMap = new LinkedHashMap<>();
+        int i = 1;
+        for (String name : filenames) {
+            sortedMap.put(i++, name);
+        }
+        for (int key : sortedMap.keySet()) {
+            System.out.println("[" + key + "]  " + sortedMap.get(key));
+        }
     }
 }
