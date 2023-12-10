be9c4d54f0d26c85d13a78eed82950a090e0a3c0
Eduard Kavalchuk <eduard_kavalchuk@epam.com>
2023-12-10 19:40:43 +0300

    Finalized developing tests for multithreaded search

--- test/home_work_6/MultiThreadedSearchTest.java
+++ test/home_work_6/MultiThreadedSearchTest.java
@@ -15,18 +15,39 @@ public class MultiThreadedSearchTest {
     String dir = Paths.get(currentDir, "resources", "books").toString();
     String output = Paths.get(currentDir, "resources", "result.txt").toString();
     static final int N_LINES = 99;
+    static final String KEYWORD = "на";
 
+    /**
+     * Проверяем, что кол-во записей в файле с результатами совпадает с кол-вом текстовых файлов в директории
+     * */
     @Test
     @DisplayName("Number of lines in output file is as expected")
     public void testNumberOfLinesInOutputFileIsAsExpected() {
-        MultiThreadedFileSearch.doSearch(dir, output, "на");
-
+        MultiThreadedFileSearch.doSearch(dir, output, KEYWORD);
         try (BufferedReader reader = new BufferedReader(new FileReader(output))) {
             int counter = 0;
             while (reader.readLine() != null) {
                 ++counter;
             }
             assertEquals(counter, N_LINES);
+        } catch (IOException e) {
+            fail();
+        }
+    }
+
+    /**
+     * Проверяем структуру файла с результатами -- в каждой записи на определённом месте должно
+     * находиться ключевое слово
+     * */
+    @Test
+    @DisplayName("Structure of output file")
+    public void testOutputFileStructure() {
+        MultiThreadedFileSearch.doSearch(dir, output, KEYWORD);
+        try (BufferedReader reader = new BufferedReader(new FileReader(output))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                assertEquals(line.split(" ")[2], KEYWORD);
+            }
         } catch (IOException e) {
             fail();
         }
